fd3f43cff73ca4ba27399d3599ec12580bbe1b2e
goncaloferreira966 <149788004+goncaloferreira966@users.noreply.github.com>
2025-03-12 12:14:58 +0000

    Template de email atualizado

--- laravel/Smart4Finances/resources/views/emails/report.blade.php
+++ laravel/Smart4Finances/resources/views/emails/report.blade.php
@@ -4,8 +4,14 @@
     <title>Relatório Smart4Finances</title>
 </head>
 <body>
-    <p>Olá,</p>
-    <p>Segue em anexo o Relatório Smart4Finances.</p>
+    <p>Caríssimo(a) Cliente,</p>
+    <p>Segue em anexo o Relatório Smart4Finances por si solicitado.</p>
     <p>Muito Obrigado pela sua Preferência!</p>
+    <br>
+    <p>A Equipa Smart4Finances</p>
+    <br>
+    <img src="{{ asset('storage/photos/logo.png') }}" alt="Smart4Finances" width="300">
+    <br>
+    <br>
 </body>
 </html>
