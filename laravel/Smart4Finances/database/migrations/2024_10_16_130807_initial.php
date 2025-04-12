9a62775d03134e25523fdd567a49835abd813468
goncaloferreira966 <149788004+goncaloferreira966@users.noreply.github.com>
2025-04-12 13:34:23 +0100

    miragtion user_id on table categories

--- laravel/Smart4Finances/database/migrations/2024_10_16_130807_initial.php
+++ laravel/Smart4Finances/database/migrations/2024_10_16_130807_initial.php
@@ -39,6 +39,7 @@ return new class extends Migration
 
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
             $table->string('name');
             $table->timestamps();
         });
