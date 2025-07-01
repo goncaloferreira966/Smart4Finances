5d859b65723afe2a7b6f0e80e19e9566c7782ae5
razorlace <59343203+razorlace@users.noreply.github.com>
2025-07-01 19:47:19 +0100

    Fix CORS configuration and add .env.jenkins for production

--- laravel/Smart4Finances/config/cors.php
+++ laravel/Smart4Finances/config/cors.php
@@ -19,7 +19,11 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => [
+        'https://cmartins.pt',
+        'http://localhost:5173',
+        'http://localhost:3000',
+    ],
 
     'allowed_origins_patterns' => [],
 
@@ -29,6 +33,6 @@ return [
 
     'max_age' => 0,
 
-    'supports_credentials' => false,
+    'supports_credentials' => true,
 
 ];
